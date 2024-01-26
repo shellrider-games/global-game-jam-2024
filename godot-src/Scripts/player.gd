22c83963b1c8886f12b18cfdac4e69c961091142
Georg Becker <georg.becker@shellrider.com>
2024-01-26 20:07:16 +0100

    Implements 8 directional normalized movement

--- godot-src/Scripts/player.gd
+++ godot-src/Scripts/player.gd
@@ -5,10 +5,25 @@ extends Node2D
 func _ready():
 	pass # Replace with function body.
 
+func _movement_dir():
+	var input_x = 0
+	if Input.is_action_pressed("move_right"):
+		input_x += 1
+	if Input.is_action_pressed("move_left"):
+		input_x -= 1
+
+	var input_y = 0
+	if Input.is_action_pressed("move_down"):
+		input_y += 1
+	if Input.is_action_pressed("move_up"):
+		input_y -= 1
+	
+	return Vector2(
+		input_x,
+		input_y
+	).normalized()
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	print(delta);
-	if Input.is_action_pressed("move_right"):
-		position.x += speed*delta
-	if Input.is_action_pressed("move_left"):
-		position.x -= speed*delta
+	position += _movement_dir()*speed*delta
+	
