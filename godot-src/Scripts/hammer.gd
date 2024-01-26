fdfec93f690731088a7c81b87b9bc3d0d9acaa13
Georg Becker <georg.becker@shellrider.com>
2024-01-26 22:27:40 +0100

    Implements hammer orbit and flip

--- godot-src/Scripts/hammer.gd
+++ godot-src/Scripts/hammer.gd
@@ -2,12 +2,20 @@ extends Node2D
 
 @export var follow_target : Node2D
 @export var offset : Vector2
+@export var sprite : Sprite2D
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+func _update_position():
+	var mouse_pos = get_viewport().get_mouse_position()
+	var hammer_dir = (follow_target.position - mouse_pos).normalized()
+	position = follow_target.position +  Vector2(0,offset.y) + \
+		hammer_dir * offset.x
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	position = follow_target.position + offset
-	
+	_update_position()
+	sprite.flip_h = follow_target.position.x <= position.x
