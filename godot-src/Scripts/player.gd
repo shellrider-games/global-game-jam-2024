a9e0754c475dc451a07aa4c2cb7ddf29da0d782c
Gluraque <maximilian.barry.mb@gmail.com>
2024-01-26 19:48:13 +0100

    Player move right implemented

--- godot-src/Scripts/player.gd
+++ godot-src/Scripts/player.gd
@@ -5,8 +5,8 @@ extends Node2D
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_pressed("MoveRight"):
+	print(delta);
+	if Input.is_action_pressed("move_right"):
 		position.x += speed*delta
