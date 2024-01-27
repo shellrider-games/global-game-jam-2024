08a803a43e05d2c41bec9df17bddd023061ac4c0
Georg Becker <georg.becker@shellrider.com>
2024-01-27 14:58:58 +0100

    Create knockback for enemies when tagged

--- godot-src/Scripts/enemy.gd
+++ godot-src/Scripts/enemy.gd
@@ -1,21 +1,33 @@
 class_name Enemy
 
-extends StaticBody2D
+extends CharacterBody2D
 
 @export var after_conversion_scene : Resource
 @export var player : Player
 @export var tag_indicator : Node2D
 
+@export var knockback_amount : float
+@export var friction : float = 0.1
+@export var move_epsilon : float = 1
+
 var target_manager : TargetManager
 
 func _ready():
-    target_manager = get_tree().root.find_child(
-        "TargetManager",
-        true,
-        false
-    )
-    
+	target_manager = get_tree().root.find_child(
+		"TargetManager",
+		true,
+		false
+	)
+	
+func knockback_from(pos : Vector2) -> void:
+	var direction = (position - pos).normalized()
+	velocity = direction * knockback_amount
 
+func target():
+	target_manager.add_target(self)
 
-func take_damage():
-    target_manager.add_target(self)
+func _physics_process(delta):
+	if velocity.length() > move_epsilon:
+		velocity = lerp(velocity, Vector2(0,0), clamp(friction*delta,0,1))
+	move_and_slide()
+	
