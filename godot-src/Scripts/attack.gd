fdf2459b3cca43f3facc77bba9cddc3c31b0617b
Georg Becker <georg.becker@shellrider.com>
2024-01-27 10:21:30 +0100

    Make enemies attackable and do some bookkeeping if stuff was already hurt by attack

--- godot-src/Scripts/attack.gd
+++ godot-src/Scripts/attack.gd
@@ -2,10 +2,19 @@ extends Node2D
 
 class_name Attack
 
+@export var collision_area : Area2D
+
+var already_attacked := []
+
+func _on_body_entered(body : Node2D):
+    if body.has_method("take_damage") and body not in already_attacked:
+        already_attacked.append(body)
+        body.take_damage()
+
 func _on_timeout() -> void:
     queue_free()
 
 func _ready():
     var timer := get_tree().create_timer(0.5)
     timer.timeout.connect(_on_timeout)
-    print("Here")
+    collision_area.body_entered.connect(_on_body_entered)
