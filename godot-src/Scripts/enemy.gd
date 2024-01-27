2e172ccca290669b28c9a4d56db3f4267f382b70
Gluraque <maximilian.barry.mb@gmail.com>
2024-01-27 19:58:08 +0100

    added health for enemy

--- godot-src/Scripts/enemy.gd
+++ godot-src/Scripts/enemy.gd
@@ -16,55 +16,58 @@ var target_manager : TargetManager
 var is_angry : bool
 var shoot_cooldown : float
 var player : Player
+var health : int = 5
 
 func knockback_from(pos : Vector2) -> void:
-    var direction = (position - pos).normalized()
-    velocity = direction * knockback_amount
+	var direction = (position - pos).normalized()
+	velocity = direction * knockback_amount
 
 func bullet_hit():
-    pass
+	health -= 1
+	if health <= 0:
+		pass
 
 func _ready():
-    target_manager = get_tree().root.find_child(
-        "TargetManager",
-        true,
-        false
-    )
-    player = get_tree().root.find_child(
-        "Player",
-        true,
-        false
-    )
-    is_angry = false
-    
+	target_manager = get_tree().root.find_child(
+		"TargetManager",
+		true,
+		false
+	)
+	player = get_tree().root.find_child(
+		"Player",
+		true,
+		false
+	)
+	is_angry = false
+	
 func target():
-    if not is_angry:
-        shoot_cooldown = time_between_shots
-    target_manager.add_target(self)
+	if not is_angry:
+		shoot_cooldown = time_between_shots
+	target_manager.add_target(self)
 
 func _spawn_bullet():
-    var bullets = get_tree().root.find_child(
-        "Bullets",
-        true,
-        false
-    )
-    var noob = bullet_scene.instantiate()
-    noob.global_position = bullet_spawn.global_position
-    noob.direction = (player.global_position - bullet_spawn.global_position) \
-        .normalized()
-    bullets.add_child(noob)
+	var bullets = get_tree().root.find_child(
+		"Bullets",
+		true,
+		false
+	)
+	var noob = bullet_scene.instantiate()
+	noob.global_position = bullet_spawn.global_position
+	noob.direction = (player.global_position - bullet_spawn.global_position) \
+		.normalized()
+	bullets.add_child(noob)
 
 
 func _process(delta):
-    tag_indicator.visible = is_angry
-    if is_angry:
-        shoot_cooldown -= delta
-        if(shoot_cooldown <= 0):
-            shoot_cooldown += time_between_shots
-            _spawn_bullet()
+	tag_indicator.visible = is_angry
+	if is_angry:
+		shoot_cooldown -= delta
+		if(shoot_cooldown <= 0):
+			shoot_cooldown += time_between_shots
+			_spawn_bullet()
 
 func _physics_process(delta):
-    if velocity.length() > move_epsilon:
-        velocity = lerp(velocity, Vector2(0,0), clamp(friction*delta,0,1))
-    move_and_slide()
-    
+	if velocity.length() > move_epsilon:
+		velocity = lerp(velocity, Vector2(0,0), clamp(friction*delta,0,1))
+	move_and_slide()
+	
