914b0a63b9b0dd459d71d6cecad8b8a9da7674da
Georg Becker <georg.becker@shellrider.com>
2024-01-27 13:52:27 +0100

    enemies display indicator if tagged

--- godot-src/Scripts/enemy.gd
+++ godot-src/Scripts/enemy.gd
@@ -4,11 +4,12 @@ extends StaticBody2D
 
 @export var after_conversion_scene : Resource
 @export var player : Player
+@export var tag_indicator : Node2D
 
 var target_manager : TargetManager
 
 func _ready():
-	target_manager = get_tree().root.find_child(
+    target_manager = get_tree().root.find_child(
         "TargetManager",
         true,
         false
@@ -17,4 +18,5 @@ func _ready():
 
 
 func take_damage():
-	target_manager.add_target(self)
+    tag_indicator.visible = true
+    target_manager.add_target(self)
