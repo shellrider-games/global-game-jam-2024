a1daed8a7f561cc54e491029dc98991eb075c7dd
Georg Becker <georg.becker@shellrider.com>
2024-01-27 10:28:30 +0100

    Makes it possible to convert enemies into minions if they get hit

--- godot-src/Scripts/enemy.gd
+++ godot-src/Scripts/enemy.gd
@@ -1,4 +1,11 @@
+class_name Enemy
+
 extends StaticBody2D
 
+@export var after_conversion_scene : Resource
+
 func take_damage():
-    print("Ouch!") 
+    var converted = after_conversion_scene.instantiate()
+    converted.position = position
+    add_sibling(converted)
+    queue_free()
